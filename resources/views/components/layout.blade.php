f89b1b367840c5adc63becc34b949f6b23265d0f
Vinicius Barizon <viniciusbarizon@nauon.com>
2021-12-02 16:49:26 +0000

    Layout

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <title>{{ __('Calculador de Coligações') }}</title>
+    </head>
+
+    <body>
+        {{ $slot }}
+    </body>
+</html>
