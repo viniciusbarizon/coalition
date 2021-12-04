4d47b098e6e1157a270bb53f28c04fff045e0f04
Vinicius Barizon <viniciusbarizon@nauon.com>
2021-12-04 23:34:08 +0000

    x-scenario.choose.main

--- resources/views/components/scenario/choose/ps.blade.php
+++ resources/views/components/scenario/choose/ps.blade.php
@@ -1,5 +1,7 @@
 <x-scenario.choose.main name="PS">
-    <div class="bg-pink-100 w-full h-full">
+    <div class="bg-pink-100 h-full w-full">
         <x-scenario.choose.percent percent="0%"/>
     </div>
+
+    <div class="bg-pink-500 h-0.5"></div>
 </x-scenario.choose.main>
