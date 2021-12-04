a6f882f8dcc05501ff0ea21dcc4b31162b1b1198
Vinicius Barizon <viniciusbarizon@nauon.com>
2021-12-04 17:05:56 +0000

    scenarios

--- resources/views/components/starting/scenarios.blade.php
+++ resources/views/components/starting/scenarios.blade.php
@@ -1,4 +1,4 @@
-<div class="max-w-screen-sm shadow-inner pt-3 px-2 space-y-2 self-end">
+<div class="max-w-screen-sm shadow-inner pt-3 px-2 space-y-2">
     <div class="flex flex-row font-semibold">
         <div>
             {{ __('Cenários') }}
@@ -21,51 +21,99 @@
 
     <div class="gap-x-2 grid grid-cols-8 text-xs w-60">
         <div>
-            <div class="text-gray-500">32%</div>
+            <div class="text-gray-500">
+                32%
+            </div>
+
             <div class="bg-pink-400 h-8"></div>
-            <div class="font-semibold">PS</div>
+
+            <div class="font-semibold">
+                PS
+            </div>
         </div>
 
         <div class="self-end">
-            <div class="text-gray-500">28%</div>
+            <div class="text-gray-500">
+                28%
+            </div>
+
             <div class="bg-yellow-400 h-6"></div>
-            <div class="font-semibold">PSD</div>
+
+            <div class="font-semibold">
+                PSD
+            </div>
         </div>
 
         <div class="self-end">
-            <div class="text-gray-500">12%</div>
+            <div class="text-gray-500">
+                12%
+            </div>
+
             <div class="bg-blue-400 h-4"></div>
-            <div class="font-semibold">CDS</div>
+
+            <div class="font-semibold">
+                CDS
+            </div>
         </div>
 
         <div class="self-end">
-            <div class="text-gray-500">12%</div>
+            <div class="text-gray-500">
+                12%
+            </div>
+
             <div class="bg-red-500 h-4"></div>
-            <div class="font-semibold">PCP</div>
+
+            <div class="font-semibold">
+                PCP
+            </div>
         </div>
 
         <div class="self-end">
-            <div class="text-gray-500">8%</div>
+            <div class="text-gray-500">
+                8%
+            </div>
+
             <div class="bg-yellow-600 h-2"></div>
-            <div class="font-semibold">BE</div>
+
+            <div class="font-semibold">
+                BE
+            </div>
         </div>
 
         <div class="self-end">
-            <div class="text-gray-500">3%</div>
+            <div class="text-gray-500">
+                3%
+            </div>
+
             <div class="bg-blue-200 h-1"></div>
-            <div class="font-semibold">IL</div>
+
+            <div class="font-semibold">
+                IL
+            </div>
         </div>
 
         <div class="self-end">
-            <div class="text-gray-500">3%</div>
+            <div class="text-gray-500">
+                3%
+            </div>
+
             <div class="bg-gray-300 h-1"></div>
-            <div class="font-semibold">C</div>
+
+            <div class="font-semibold">
+                C
+            </div>
         </div>
 
         <div class="self-end">
-            <div class="text-gray-500">3%</div>
+            <div class="text-gray-500">
+                3%
+            </div>
+
             <div class="bg-gray-600 h-1"></div>
-            <div class="font-semibold">...</div>
+
+            <div class="font-semibold">
+                ...
+            </div>
         </div>
     </div>
 </div>
