9d2b5b116639b58a4250793e04dd71b945b4467b
Vinicius Barizon <viniciusbarizon@nauon.com>
2021-12-04 12:48:48 +0000

    Coligações possíveis

--- resources/views/calculator.blade.php
+++ resources/views/calculator.blade.php
@@ -1,7 +1,25 @@
 <x-layout>
-    <div class="pl-2 pt-2 w-full" id="starting">
-        <div class="font-semibold h-12 text-lg">
-            {{ __('Calculador de coligações') }}
+    <div class="pt-2 px-2 space-y-4 w-full" id="starting">
+        <div class="space-y-2">
+            <div class="font-semibold h-6 text-xl">
+                <div>{{ __('Calculador de coligações') }}</div>
+            </div>
+
+            <div class="text-xs">
+                {{ __('Faltam semanas para as eleições. Para governar, é conveniente ter uma maioria.
+                    Escolha ou crie o seu próprio cenário.')
+                }}
+            </div>
+        </div>
+
+        <div class="space-y-4">
+            <div class="font-semibold text-sm">
+                {{ __('Coligações possíveis')}}
+            </div>
+
+            <div>
+                <div class="bg-pink-400 rounded-xl text-center text-sm text-white w-8">PS</div>
+            </div>
         </div>
     </div>
 </x-layout>
