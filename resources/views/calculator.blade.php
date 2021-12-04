8a7e78b1047421ba398c1448c0ea4deaab225be3
Vinicius Barizon <viniciusbarizon@nauon.com>
2021-12-04 13:02:37 +0000

    O Bloco central

--- resources/views/calculator.blade.php
+++ resources/views/calculator.blade.php
@@ -17,8 +17,21 @@
                 {{ __('Coligações possíveis')}}
             </div>
 
-            <div>
-                <div class="bg-pink-400 rounded-xl text-center text-sm text-white w-8">PS</div>
+            <div class="space-y-1 text-sm">
+                <div class="flex flex-row space-x-2">
+                    <div class="bg-pink-400 rounded-xl text-center text-white w-12">PS</div>
+                    <div>+</div>
+                    <div class="bg-pink-400 rounded-xl text-center text-sm text-white w-12">PSD</div>
+                </div>
+
+                <div class="space-y-1.5 text-xs">
+                    <div class="font-semibold">{{ __('O Bloco central') }}</div>
+                    <div class="text-gray-400">{{ __('Probabilidade: euromilhões') }}</div>
+                    <div>
+                        {{ __('Uma reedição da década dos 80, em que Costa e Rio se teriam que entender sobre quem de facto
+                            será primeiro ministro.') }}
+                    </div>
+                </div>
             </div>
         </div>
     </div>
