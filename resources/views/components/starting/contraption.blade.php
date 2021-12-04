2a856629944199d6a4376a81651657fccb7cfbee
Vinicius Barizon <viniciusbarizon@nauon.com>
2021-12-04 16:58:18 +0000

    x-starting.contraption

---
+++ resources/views/components/starting/contraption.blade.php
@@ -0,0 +1,31 @@
+<div class="space-y-1 text-xs">
+    <div class="flex flex-row space-x-1">
+        <x-starting.ps-rounded/>
+        <x-starting.plus/>
+
+        <div class="bg-red-500 py-0.5 rounded-3xl text-center text-white w-12">
+            PCP
+        </div>
+
+        <x-starting.plus/>
+
+        <div class="bg-yellow-600 py-0.5 rounded-3xl text-center text-white w-12">
+            BE
+        </div>
+    </div>
+
+    <div class="space-y-1.5 ">
+        <div class="font-semibold">
+            {{ __('Geringonça 2.0') }}
+        </div>
+
+        <div class="text-gray-500">
+            {{ __('Probabilidade: já se viu mais estranho') }}
+        </div>
+
+        <div>
+            {{ __('Não tendo havido acordo para o Orçamento de Estado, talvez se encontre acordo para uma
+                governação.') }}
+        </div>
+    </div>
+</div>
