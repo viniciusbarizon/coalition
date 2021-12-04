4756efcddfdb51315160f9cad837e9ea0839fcc9
Vinicius Barizon <viniciusbarizon@nauon.com>
2021-12-04 18:38:46 +0000

    x-scenarios.update

--- resources/views/components/scenarios/main.blade.php
+++ resources/views/components/scenarios/main.blade.php
@@ -4,15 +4,7 @@
             {{ __('Cenários') }}
         </div>
 
-        <div class="pr-2 text-blue-300 text-right w-full">
-            {{ __('alterar') }}
-        </div>
-
-        <div class="pt-0.5 text-blue-300">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-            </svg>
-        </div>
+        <x-scenarios.update/>
     </div>
 
     <div class="text-xs">
