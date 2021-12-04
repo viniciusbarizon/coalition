ca0b8d558f7f9d4f1b91b4298f6732caa81708cc
Vinicius Barizon <viniciusbarizon@nauon.com>
2021-12-04 12:27:46 +0000

    Calculador de coligações

--- resources/views/calculator.blade.php
+++ resources/views/calculator.blade.php
@@ -1,3 +1,7 @@
 <x-layout>
-    aaa
+    <div class="pl-2 pt-2 w-full" id="starting">
+        <div class="font-semibold h-12 text-lg">
+            {{ __('Calculador de coligações') }}
+        </div>
+    </div>
 </x-layout>
