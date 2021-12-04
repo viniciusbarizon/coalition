aeb09a0affc9747a340279d5b730eb83662ab3cf
Vinicius Barizon <viniciusbarizon@nauon.com>
2021-12-04 13:49:44 +0000

    Geringonça 2.0

--- resources/views/calculator.blade.php
+++ resources/views/calculator.blade.php
@@ -20,13 +20,23 @@
             <div class="space-y-1 text-xs">
                 <div class="flex flex-row space-x-1">
                     <x-ps-rounded/>
+
                     <x-plus/>
-                    <div class="bg-yellow-400 py-0.5 rounded-3xl text-center text-white w-12">PSD</div>
+
+                    <div class="bg-yellow-400 py-0.5 rounded-3xl text-center text-white w-12">
+                        PSD
+                    </div>
                 </div>
 
                 <div class="space-y-1.5 ">
-                    <div class="font-semibold">{{ __('O Bloco central') }}</div>
-                    <div class="text-gray-400">{{ __('Probabilidade: euromilhões') }}</div>
+                    <div class="font-semibold">
+                        {{ __('O Bloco central') }}
+                    </div>
+
+                    <div class="text-gray-400">
+                        {{ __('Probabilidade: euromilhões') }}
+                    </div>
+
                     <div>
                         {{ __('Uma reedição da década dos 80, em que Costa e Rio se teriam que entender sobre quem de facto
                             será primeiro ministro.') }}
@@ -37,9 +47,33 @@
             <div class="space-y-1 text-xs">
                 <div class="flex flex-row space-x-1">
                     <x-ps-rounded/>
+
                     <x-plus/>
-                    <div class="bg-red-500 py-0.5 rounded-3xl text-center text-white w-12">PCP</div>
+
+                    <div class="bg-red-500 py-0.5 rounded-3xl text-center text-white w-12">
+                        PCP
+                    </div>
+
                     <x-plus/>
+
+                    <div class="bg-yellow-600 py-0.5 rounded-3xl text-center text-white w-12">
+                        BE
+                    </div>
+                </div>
+
+                <div class="space-y-1.5 ">
+                    <div class="font-semibold">
+                        {{ __('Geringonça 2.0') }}
+                    </div>
+
+                    <div class="text-gray-400">
+                        {{ __('Probabilidade: já se viu mais estranho') }}
+                    </div>
+
+                    <div>
+                        {{ __('Não tendo havido acordo para o Orçamento de Estado, talvez se encontre acordo para uma
+                            governação.') }}
+                    </div>
                 </div>
             </div>
         </div>
