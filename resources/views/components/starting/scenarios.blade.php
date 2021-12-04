489bc3e7e92225c18cc7bd02e81ae204342f3bf1
Vinicius Barizon <viniciusbarizon@nauon.com>
2021-12-04 16:55:54 +0000

    x-starting.scenarios

---
+++ resources/views/components/starting/scenarios.blade.php
@@ -0,0 +1,71 @@
+<div class="max-w-screen-sm shadow-inner pt-3 px-2 space-y-2">
+    <div class="flex flex-row font-semibold">
+        <div>
+            {{ __('Cenários') }}
+        </div>
+
+        <div class="pr-2 text-blue-300 text-right w-full">
+            {{ __('alterar') }}
+        </div>
+
+        <div class="pt-0.5 text-blue-300">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+            </svg>
+        </div>
+    </div>
+
+    <div class="text-xs">
+        {{ __('Resultado legislativas 2019') }}
+    </div>
+
+    <div class="gap-x-2 grid grid-cols-8 text-xs w-60">
+        <div>
+            <div class="text-gray-500">32%</div>
+            <div class="bg-pink-400 h-8"></div>
+            <div class="font-semibold">PS</div>
+        </div>
+
+        <div class="self-end">
+            <div class="text-gray-500">28%</div>
+            <div class="bg-yellow-400 h-6"></div>
+            <div class="font-semibold">PSD</div>
+        </div>
+
+        <div class="self-end">
+            <div class="text-gray-500">12%</div>
+            <div class="bg-blue-400 h-4"></div>
+            <div class="font-semibold">CDS</div>
+        </div>
+
+        <div class="self-end">
+            <div class="text-gray-500">12%</div>
+            <div class="bg-red-500 h-4"></div>
+            <div class="font-semibold">PCP</div>
+        </div>
+
+        <div class="self-end">
+            <div class="text-gray-500">8%</div>
+            <div class="bg-yellow-600 h-2"></div>
+            <div class="font-semibold">BE</div>
+        </div>
+
+        <div class="self-end">
+            <div class="text-gray-500">3%</div>
+            <div class="bg-blue-200 h-1"></div>
+            <div class="font-semibold">IL</div>
+        </div>
+
+        <div class="self-end">
+            <div class="text-gray-500">3%</div>
+            <div class="bg-gray-300 h-1"></div>
+            <div class="font-semibold">C</div>
+        </div>
+
+        <div class="self-end">
+            <div class="text-gray-500">3%</div>
+            <div class="bg-gray-600 h-1"></div>
+            <div class="font-semibold">...</div>
+        </div>
+    </div>
+</div>
