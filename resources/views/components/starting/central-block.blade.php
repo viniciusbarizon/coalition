40433f630fa408a4bcde611b8b8a979579b33aa4
Vinicius Barizon <viniciusbarizon@nauon.com>
2021-12-04 16:59:41 +0000

    x-starting.central-block

---
+++ resources/views/components/starting/central-block.blade.php
@@ -0,0 +1,25 @@
+<div class="space-y-1 text-xs">
+    <div class="flex flex-row space-x-1">
+        <x-starting.ps-rounded/>
+        <x-starting.plus/>
+
+        <div class="bg-yellow-400 py-0.5 rounded-3xl text-center text-white w-12">
+            PSD
+        </div>
+    </div>
+
+    <div class="space-y-1.5 ">
+        <div class="font-semibold">
+            {{ __('O Bloco central') }}
+        </div>
+
+        <div class="text-gray-500">
+            {{ __('Probabilidade: euromilhões') }}
+        </div>
+
+        <div>
+            {{ __('Uma reedição da década dos 80, em que Costa e Rio se teriam que entender sobre quem de facto
+                será primeiro ministro.') }}
+        </div>
+    </div>
+</div>
