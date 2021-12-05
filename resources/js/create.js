f18a5107e194a6b5bbf96ee1164b387e81955d81
Vinicius Barizon <viniciusbarizon@nauon.com>
2021-12-05 20:57:27 +0000

    js

--- resources/js/create.js
+++ resources/js/create.js
@@ -1,24 +1,3 @@
-document.getElementById("update-text").addEventListener("click", update);
-document.getElementById("update-svg").addEventListener("click", update);
-
-document.getElementById("close-text").addEventListener("click", close);
-document.getElementById("close-svg").addEventListener("click", close);
-
-var expandedStyles = document.getElementById('expanded').style;
-var startingStyles = document.getElementById('starting').style;
-
-function update()
-{
-    startingStyles.display = "none";
-    expandedStyles.display = "block";
-}
-
-function close()
-{
-    expandedStyles.display = "none";
-    startingStyles.display = "block";
-}
-
 function increase(partyId)
 {
     let percentageOthers = getPercentage('others');
