ace239fd31d3f6042e9af3b9ebb939dbc2d9f711
Vinicius Barizon <viniciusbarizon@nauon.com>
2021-12-05 16:13:16 +0000

    x-scenario.create.party

--- resources/views/components/scenario/create/main.blade.php
+++ resources/views/components/scenario/create/main.blade.php
@@ -9,17 +9,54 @@
     </div>
 
     <div class="gap-x-2 grid grid-cols-8 text-xs">
-        <x-scenario.create.party background="bg-pink-400" backgroundLight="bg-pink-100" id="ps" name="PS"
+        <x-scenario.create.party
+            background="bg-pink-400"
+            backgroundLight="bg-pink-100"
+            id="ps"
+            name="PS"
             percentage="0%"/>
 
-        <x-scenario.create.party background="bg-yellow-400" backgroundLight="bg-yellow-100" id="psd" name="PSD"/>
-        <x-scenario.create.party background="bg-blue-400" backgroundLight="bg-blue-100" id="cds" name="CDS"/>
-        <x-scenario.create.party background="bg-red-600" backgroundLight="bg-red-100" id="pcp" name="PCP"/>
-        <x-scenario.create.party background="bg-red-400" backgroundLight="bg-red-100" id="be" name="BE"/>
-        <x-scenario.create.party background="bg-blue-200" backgroundLight="bg-blue-100" id="il" name="IL"/>
-        <x-scenario.create.party background="bg-gray-300" backgroundLight="bg-gray-100" id="c" name="C"/>
+        <x-scenario.create.party
+            background="bg-yellow-400"
+            backgroundLight="bg-yellow-100"
+            id="psd"
+            name="PSD"/>
 
-        <x-scenario.create.party background="bg-gray-600" backgroundLight="bg-gray-100" id="others" name="..."
+        <x-scenario.create.party
+            background="bg-blue-400"
+            backgroundLight="bg-blue-100"
+            id="cds"
+            name="CDS"/>
+
+        <x-scenario.create.party
+            background="bg-red-600"
+            backgroundLight="bg-red-100"
+            id="pcp"
+            name="PCP"/>
+
+        <x-scenario.create.party
+            background="bg-red-400"
+            backgroundLight="bg-red-100"
+            id="be"
+            name="BE"/>
+
+        <x-scenario.create.party
+            background="bg-blue-200"
+            backgroundLight="bg-blue-100"
+            id="il"
+            name="IL"/>
+
+        <x-scenario.create.party
+            background="bg-gray-300"
+            backgroundLight="bg-gray-100"
+            id="c"
+            name="C"/>
+
+        <x-scenario.create.party
+            background="bg-gray-600"
+            backgroundLight="bg-gray-100"
+            id="others"
+            name="..."
             percentage="100"/>
     </div>
 </footer>
